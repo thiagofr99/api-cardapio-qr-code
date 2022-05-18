58ea041e1e0496daa9b2650dd8b32e0a8791ca62
thiagofr99 <linkinht@gmail.com>
2022-05-17 23:40:46 -0300

    fix(Empresa): alterado a busca para uma nova página assim poluindo menos a tela principal e aumentando a quantidade de resultado por página.

---
+++ Front/client/src/pages/EmpresaTodos/index.js
@@ -0,0 +1,138 @@
+import React,{useState, useEffect} from "react";
+import { Link, useHistory} from "react-router-dom";
+import InputMask from "react-input-mask";
+
+import './style.css';
+
+import api from '../../services/api'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub, faYoutube } from '@fortawesome/free-brands-svg-icons'
+
+
+
+export default function EmpresaTodos(){
+
+    const [permissionsReturn, setPermissionsReturn] = useState([]);
+    const [permissionsResponse, setPermissionsResponse] =useState([]);
+
+    const [empresaNome, setEmpresaNome] = useState('');
+    const [cepMask, setCepMask] = useState('');
+    const [numero, setNumero] = useState('');    
+    const [complemento, setComplemento] = useState('');
+
+    
+    const [empresas, setEmpresas] = useState([]);  
+
+    const accessToken = sessionStorage.getItem('accessToken');
+    
+
+    const history = useHistory();
+
+    useEffect(()=> {
+        findAllByEmpresaName();
+    },[]);
+
+
+    async function findAllByEmpresaName(){
+
+
+        try{
+    
+            const response = await api.get('api/empresa/v1/findAllByEmpresaName/?page=0&limit=10&ASC',{                
+              headers:{
+                  Authorization: `Bearer ${accessToken}`
+              }
+            }).then(responses=> {
+                setEmpresas(responses.data._embedded.empresaVoes)
+            })
+            
+              
+            alert('Busca realizada com sucesso.')          
+      
+          } catch (err){
+            alert('Erro ao buscar registros!'+err)
+          }
+        
+
+    }  
+    
+
+    return (
+        <div id="container">
+           
+            <header>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/usuario"> 
+                                Usuarios
+                            </Link>     
+                            </li>
+                            <li> <Link className="active" to="/empresa"> 
+                                Empresas
+                            </Link>     
+                        </li>               
+                    </ul>                
+                    <div id="cabecalho" className="flex">
+                        <a className="linkedin-cab" href="https://www.linkedin.com/in/dev-thiago-furtado/">
+                            <FontAwesomeIcon icon={faLinkedin} className="linkedin" />
+                            <h2>@DEVTHIAGOFURTADO</h2>
+                        </a>                        
+                    </div>
+                    
+                </nav>
+            </header>
+            <body>          
+                <div id="lista-1">
+                <table>
+                    <tr>
+                        <th>Nome da Empresa</th>
+                        <th>CEP</th>
+                        <th>Numero</th>
+                        <th>Complemento</th>
+                        <th>Data Cadastro</th>                        
+                        <th>Ações</th>
+                    </tr>
+                    {empresas.map( p=>(
+                    <tr>
+                        <td> {p.empresaNome} </td>
+                        <td> {p.cep} </td>
+                        <td> {p.numero} </td>
+                        <td> {p.complemento} </td>
+                        <td> {p.dataCadastro} </td>                        
+                        <td>
+                            <button onClick={""} className="input-button-4" type="submit" >Deletar</button>
+                            <button onClick={""} className="input-button-4" type="submit" >Renovar</button>
+                            <button onClick={""} className="input-button-4" type="submit" >Alterar</button>
+                            <button onClick={""} className="input-button-4" type="submit" >Desabilitar</button>
+                        </td>
+                    </tr>                 
+                                ))}
+                    
+                </table>
+
+                </div>
+
+            </body>
+            <footer>
+                <div className="dados-pessoais">
+                    <div className="endereco">
+                        <h2> Granja Portugal </h2>
+                        <h3> Fortaleza - CE</h3>  
+                    </div>
+                    <div className="rede-social">                        
+                        <a className="link-href" href="https://github.com/thiagofr99"><FontAwesomeIcon icon={faGithub} className="github" /><h3>/thiagofr99</h3></a> 
+                        <a className="link-href-yt" href="https://www.youtube.com/channel/UCxxFrDeO_yXxRe7EB5aTjfA"><FontAwesomeIcon icon={faYoutube} className="youtube" /><h3>Thiago Furtado</h3></a> 
+                    </div>
+                                   
+                </div>
+                <div className="copyright">
+                        Copyright © www.devthiagofurtado.com 2022
+                    </div> 
+               
+            </footer>
+        </div>
+        
+    );
+}
